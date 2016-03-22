0cfa325d77bd6477fff979be927722940e532fe6
Suresh kumar <sureshssk2006@ymail.com>
2016-03-22 06:52:35 +0530

    Feat: Initial Hello world app

---
+++ app/src/main/java/com/sureshssk2006/gmail/popularmovies/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sureshssk2006.gmail.popularmovies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
