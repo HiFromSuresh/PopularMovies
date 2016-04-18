ef7b85a724390cab7d27b62ae7998afd13a1acbe
Suresh kumar <sureshssk2006@ymail.com>
2016-04-18 18:04:39 +0530

    Feat: Open details fragment from detailsActivity

--- app/src/main/java/com/sureshssk2006/gmail/popularmovies/DetailsActivity.java
+++ app/src/main/java/com/sureshssk2006/gmail/popularmovies/DetailsActivity.java
@@ -1,9 +1,13 @@
 package com.sureshssk2006.gmail.popularmovies;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class DetailsActivity extends AppCompatActivity {
+
+    private static final String ACTIVITYKEY = "activity_key";
+    private static final String OBJECT_KEY = "object_key";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,9 +16,13 @@ public class DetailsActivity extends AppCompatActivity {
 
         if(savedInstanceState == null){
             DetailsFragment detailsFragment = new DetailsFragment();
+            Bundle args = new Bundle();
+            Intent intent = getIntent();
+            TMDBmovieList.TmdbMovee movie = (TMDBmovieList.TmdbMovee)intent.getParcelableExtra(ACTIVITYKEY);
+            args.putParcelable(OBJECT_KEY, movie);
+            detailsFragment.setArguments(args);
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.details_container, detailsFragment)
-                    .addToBackStack(null)
                     .commit();
         }
     }
--- app/src/main/java/com/sureshssk2006/gmail/popularmovies/MainActivity.java
+++ app/src/main/java/com/sureshssk2006/gmail/popularmovies/MainActivity.java
@@ -1,12 +1,15 @@
 package com.sureshssk2006.gmail.popularmovies;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements MovieListFragment.Callbackk {
 
     private static final String DETAIL_FRAG = "DFTAG";
+    private static final String KEY = "key";
+    private static final String DETAILFRAGMENT_TAG = "DFTAG";
+    private static final String ACTIVITYKEY = "activity_key";
     private boolean mTwoPane;
 
     @Override
@@ -28,6 +31,21 @@ public class MainActivity extends AppCompatActivity implements MovieListFragment
 
     @Override
     public void onItemSelected(TMDBmovieList.TmdbMovee movie) {
-        Toast.makeText(this, movie.getOriginal_title(), Toast.LENGTH_SHORT).show();
+        if (mTwoPane) {
+
+            Bundle args = new Bundle();
+            args.putParcelable(KEY, movie);
+
+            DetailsFragment detailsFragment = new DetailsFragment();
+            detailsFragment.setArguments(args);
+
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.details_container, detailsFragment, DETAILFRAGMENT_TAG)
+                    .commit();
+        } else {
+            Intent intent = new Intent(this, DetailsActivity.class);
+            intent.putExtra(ACTIVITYKEY, movie);
+            startActivity(intent);
+        }
     }
 }
